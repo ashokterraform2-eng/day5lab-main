953f4d5a0d4d5c90db70548efa232789a7e66a1c
Sai Kumar <ashok.terraform2@gmail.com>
2025-11-07 16:53:11 +0530

    latest changes

--- main.tf
+++ main.tf
@@ -168,6 +168,14 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "TEMP direct HTTP test access"
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
